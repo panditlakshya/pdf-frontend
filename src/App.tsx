2b5dc752d9b8c7b4979b0299441c53a6c8674a5f
Lakshya Pandit <lpandit@growthzilla.com>
2024-10-27 11:26:55 +0545

    set up shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-// import { useState } from "react";
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="h-screen">
@@ -11,7 +9,9 @@ function App() {
           <div className="border border-red-500 flex-1">1</div>
           <div className="md:pt-0 md:border-transparent w-full">
             <div className="m-auto text-base px-3 w-full md:px-5 lg:px-4 xl:px-5">
-              <div>2</div>
+              <div>
+                <Button>Click me</Button>
+              </div>
             </div>
           </div>
         </div>
